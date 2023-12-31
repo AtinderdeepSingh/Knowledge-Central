f7eff23dd006145904c17c4751d9a4fced4a04ba
AtinderdeepSingh <atindersaini10@gmail.com>
2023-12-30 20:50:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import pandas as pd
-from annotated_text import annotated_text
 
 load_dotenv()
 
